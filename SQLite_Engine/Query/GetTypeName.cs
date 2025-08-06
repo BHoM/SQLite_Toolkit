41b5284619d42de1d7513c98de494a62d134019d
Peter Nugent <Peter.Nugent@burohappold.com>
2025-08-06 16:12:03 +0100

    Create GetTypeName.cs

---
+++ SQLite_Engine/Query/GetTypeName.cs
@@ -0,0 +1,76 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2025, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Base.Attributes;
+using Microsoft.Data.Sqlite;
+using System;
+using System.ComponentModel;
+
+namespace BH.Engine.SQLite
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        [Description("Gets the .NET type name associated with a table name from the __Types table.")]
+        [Input("connection", "Active SQLite database connection.")]
+        [Input("tableName", "The table name to look up.")]
+        [Output("typeName", "The full .NET type name if found, null otherwise.")]
+        public static string GetTypeName(this SqliteConnection connection, string tableName)
+        {
+            if (connection == null || string.IsNullOrWhiteSpace(tableName))
+                return null;
+
+            try
+            {
+                string selectSql = @"
+                    SELECT FullTypeName 
+                    FROM __Types 
+                    WHERE TableName = @TableName";
+
+                using (var command = new SqliteCommand(selectSql, connection))
+                {
+                    command.Parameters.AddWithValue("@TableName", tableName);
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return reader.GetString(0);
+                        }
+                    }
+                }
+
+                return null;
+            }
+            catch (Exception ex)
+            {
+                BH.Engine.Base.Compute.RecordError($"Failed to get type name for table '{tableName}': {ex.Message}");
+                return null;
+            }
+        }
+
+        /***************************************************/
+    }
+}
